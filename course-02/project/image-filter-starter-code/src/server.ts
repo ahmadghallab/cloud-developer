25662d033f53db68726cbbd1a7534fec917373c8
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2019-11-04 08:30:33 +0200

    Add express filteredimage endpoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,6 +13,25 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+  app.get('/filteredimage/', async ( req, res ) => {
+    let { image_url } = req.query;
+
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`image url is required`);
+    }
+
+    let filteredpath : string = await filterImageFromURL(image_url);
+
+    res.sendFile(filteredpath, async (error) => {
+      if (error) {
+        res.status(500).end();
+      } else {
+        await deleteLocalFiles([filteredpath]);
+      }
+    });
+  });
+
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
